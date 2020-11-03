80e9f829f4c966392bf169069a5dd88145cb3601
Selçuk Karakayalı <skarakayali@gmail.com>
2020-11-03 13:24:23 +0300

    logback and base solution class

---
+++ src/main/java/com/karakays/leetcode/solutions/Base.java
@@ -0,0 +1,11 @@
+package com.karakays.leetcode.solutions;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Level;
+
+public abstract class Base {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+}
